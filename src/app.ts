f989c8bdff2ef9b9670eba5d765bf96465bebbfc
Talat09 <talat181008312@gmail.com>
2023-06-11 02:09:00 +0600

    [done] create mvc pattern for academic semester & academic faculty

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,33 @@
-import express, { Application, Request, Response } from 'express'
-import cors from 'cors'
-import usersRouter from './app/modules/users/user.route'
-const app: Application = express()
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import routes from './app/routes';
+import httpStatus from 'http-status';
+const app: Application = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-//application route
-app.use('/api/v1/users', usersRouter)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/', routes);
 //Testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working successfully')
-})
-
-export default app
+// app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   throw new Error('Testing Error logger')
+// })
+//global error handler
+app.use(globalErrorHandler);
+//handle not found route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+export default app;
